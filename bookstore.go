fa24a5919761d57df3b1813b2f053d3f46452bc4
João Octávio Santiago <jomsoutosantiago@gmail.com>
2024-08-28 00:17:13 -0300

    Implemented the "Buy" function and its tests

--- bookstore.go
+++ bookstore.go
@@ -1,7 +1,19 @@
 package bookstore
 
+import "errors"
+
 type Book struct {
-	Title string
+	Title  string
 	Author string
 	Copies int
+}
+
+func Buy(b Book) (Book, error) {
+	if b.Copies < 1 {
+		return Book{}, errors.New("mo copies left")
+	}
+
+	b.Copies--
+
+	return b, nil
 }
--- bookstore_test.go
+++ bookstore_test.go
@@ -14,3 +14,41 @@ func TestBook(t *testing.T) {
 		Copies: 99,
 	}
 }
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+
+	want := 1
+	result, err := bookstore.Buy(b)
+	got := result.Copies
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if want != got {
+		t.Errorf("want %d copies after buying 1 copy from a stock of %d, got %d", want, b.Copies, got)
+	}
+}
+
+func TestBuyErrorsIfNoBookLeft(t *testing.T) {
+	t.Parallel()
+
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+
+	_, err := bookstore.Buy(b)
+
+	if err == nil {
+		t.Error("want error buying from zero copies, got nil")
+	}
+}
